ce57051c02cc8f1104bc6aa9924a0012585f31c6
Nikolai Treschow Kühl <s221088@dtu.dk>
2025-03-18 09:48:22 +0100

    SImplified_load_data_function.py

--- load_data_function.py
+++ load_data_function.py
@@ -1,29 +1,16 @@
 from Data_processesing_class import AudioSpeechNoise_data_load, fNIRS_full_motor_data_load, fNIRS_motor_data_load, fNIRS_Alexandros_DoC_data_load, fNIRS_Alexandros_Healthy_data_load, fNIRS_CUH_patient_data_load
 
+data_loaders = {
+    "fNIrs_motor": fNIRS_motor_data_load,
+    "AudioSpeechNoise": AudioSpeechNoise_data_load,
+    "fNirs_motor_full_data": fNIRS_full_motor_data_load,
+    "fNIRS_Alexandros_DoC_data": fNIRS_Alexandros_DoC_data_load,
+    "fNIRS_Alexandros_Healthy_data": fNIRS_Alexandros_Healthy_data_load,
+    "fNIRS_CUH_patient_data": fNIRS_CUH_patient_data_load
+}
+
 def load_data(data_set : str, short_channel_correction : bool = None, negative_correlation_enhancement : bool = None, individuals :bool = False, interpolate_bad_channels:bool=False):
-    if data_set not in ("fNIrs_motor", "AudioSpeechNoise", "fNirs_motor_full_data", "fNIRS_Alexandros_DoC_data", "fNIRS_Alexandros_Healthy_data"):
+    if data_set not in data_loaders:
         raise ValueError("Dataset does not exist.")
-    
-    if data_set == "fNIrs_motor":
-        all_epochs, data_name, all_data, all_freq, data_types, individual_data = fNIRS_motor_data_load(short_channel_correction = short_channel_correction, negative_correlation_enhancement = negative_correlation_enhancement, individuals = individuals, interpolate_bad_channels=interpolate_bad_channels).load_data()
-        return all_epochs, data_name, all_data, all_freq, data_types, individual_data
-    
-    if data_set == "AudioSpeechNoise":
-        all_epochs, data_name, all_data, all_freq, data_types, individual_data = AudioSpeechNoise_data_load(short_channel_correction = short_channel_correction, negative_correlation_enhancement = negative_correlation_enhancement, individuals = individuals, interpolate_bad_channels=interpolate_bad_channels).load_data()
-        return all_epochs, data_name, all_data, all_freq, data_types, individual_data
-    
-    if data_set ==  "fNirs_motor_full_data":
-        all_epochs, data_name, all_data, all_freq, data_types, individual_data = fNIRS_full_motor_data_load(short_channel_correction = short_channel_correction, negative_correlation_enhancement = negative_correlation_enhancement, individuals = individuals, interpolate_bad_channels=interpolate_bad_channels).load_data()
-        return all_epochs, data_name, all_data, all_freq, data_types, individual_data
-    
-    if data_set ==  "fNIRS_Alexandros_DoC_data":
-        all_epochs, data_name, all_data, all_freq, data_types, individual_data = fNIRS_Alexandros_DoC_data_load(short_channel_correction = short_channel_correction, negative_correlation_enhancement = negative_correlation_enhancement, individuals = individuals, interpolate_bad_channels=interpolate_bad_channels).load_data()
-        return all_epochs, data_name, all_data, all_freq, data_types, individual_data
-    
-    if data_set ==  "fNIRS_Alexandros_Healthy_data":
-        all_epochs, data_name, all_data, all_freq, data_types, individual_data = fNIRS_Alexandros_Healthy_data_load(short_channel_correction = short_channel_correction, negative_correlation_enhancement = negative_correlation_enhancement, individuals = individuals, interpolate_bad_channels=interpolate_bad_channels).load_data()
-        return all_epochs, data_name, all_data, all_freq, data_types, individual_data
-    
-    if data_set ==  "fNIRS_CUH_patient_data":
-        all_epochs, data_name, all_data, all_freq, data_types, individual_data = fNIRS_CUH_patient_data_load(short_channel_correction = short_channel_correction, negative_correlation_enhancement = negative_correlation_enhancement, individuals = individuals, interpolate_bad_channels=interpolate_bad_channels).load_data()
-        return all_epochs, data_name, all_data, all_freq, data_types, individual_data
+    loader = data_loaders[data_set](short_channel_correction = short_channel_correction, negative_correlation_enhancement = negative_correlation_enhancement, individuals = individuals, interpolate_bad_channels=interpolate_bad_channels)
+    return loader.load_data()
