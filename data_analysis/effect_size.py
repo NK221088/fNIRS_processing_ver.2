438477e08833c901bec15c102623ab81a6b07c66
Nikolai Treschow Kühl <s221088@dtu.dk>
2025-08-25 16:50:40 +0200

    Fixed minor issues and most importantly the way we compute the means

--- data_analysis/effect_size.py
+++ data_analysis/effect_size.py
@@ -12,16 +12,16 @@ def compute_effect_size(class_instance):
     tmax = 12.5
     channels = mne_nirs.channels.get_long_channels(class_instance.Individual_participants[0].raw_haemo).info["ch_names"]
     
-    
-    
     def _compute_effect_size(raw_epochs, epochs, tmin, tmax, channels):
 
         pause_id = class_instance.standard_event_ids["Pause"]
         
         participants_session_data = {}
-        channel_means = defaultdict(lambda: defaultdict(list))
+        cond_session_means = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+        
         for index, epoch in enumerate(epochs):
-            pause_indices = np.where(raw_epochs[index].events[:, 2] == pause_id)
+            pid = f"participant_{index}"
+            pause_indices = np.where(raw_epochs[index].events[:, 2] == pause_id)[0]
             pause_times = raw_epochs[index].events[pause_indices, 0]
             pause_times = np.append(pause_times, raw_epochs[index].events[-1][0])
             start_time = 0
@@ -31,22 +31,24 @@ def compute_effect_size(class_instance):
             for pause_time in pause_times:    
                 event_indices = np.where((epoch.events[:,0] > start_time) & (epoch.events[:,0] < pause_time))
                 session = epoch[event_indices[0]]
-                session_data.append(session.crop(tmin, tmax) if len(session.event_id) == len(class_instance.data_types) else []) 
+                sess = session.crop(tmin, tmax)
+                if len(sess.event_id) == len(class_instance.data_types):
+                    session_data.append(sess)
                 start_time = pause_time
 
             
             session_differences = []
             for session in session_data:
-                if len(session) > 0:
+                if (len(session[class_instance.data_types[0]]) > 0) and (len(session[class_instance.data_types[1]]) > 0):
                     ch_differences = {}
                     for channel in channels:
-                        means = []
+                        means = {}
                         for data_type in class_instance.data_types:
-                            mean_each_epoch = np.mean(session[data_type].pick(channel).get_data(), axis=2)
+                            mean_each_epoch = np.mean(session[data_type].pick(channel).get_data(), axis=2).ravel()
                             mean_data_type = np.mean(mean_each_epoch)
-                            channel_means[channel][data_type].extend(mean_each_epoch)
-                            means.append(mean_data_type)  
-                        session_difference = means[0] - means[1]
+                            cond_session_means[data_type][channel][pid].append(mean_data_type)
+                            means[data_type] = mean_data_type  
+                        session_difference = means[class_instance.data_types[1 - np.where(np.array(class_instance.data_types) == "Control")[0][0]]] - means[class_instance.data_types[np.where(np.array(class_instance.data_types) == "Control")[0][0]]] # Always subtract the control from the active epochs
                         ch_differences[channel] = session_difference
                     session_differences.append(ch_differences)
 
@@ -54,57 +56,68 @@ def compute_effect_size(class_instance):
         
             averages_over_sessions = {}
             for channel in channels:
-                channel_sum = 0
-                for session in session_differences:
-                    channel_sum += session[channel]
-                averages_over_sessions[channel] = channel_sum / len(session_differences)
+                averages_over_sessions[channel] = np.mean([sd[channel] for sd in session_differences])
             
             participant_data["averages_over_sessions"] = averages_over_sessions
             
             participants_session_data[f"participant_{index}"] = participant_data
 
+        condition_means = {cond: {} for cond in class_instance.data_types}
+        for cond in class_instance.data_types:
+            for ch in channels:
+                per_person = []
+                for pid, sess_vals in cond_session_means[cond][ch].items():
+                    if len(sess_vals) > 0:
+                        per_person.append(np.mean(sess_vals))  # equal-weight sessions within person
+                condition_means[cond][ch] = float(np.mean(per_person)) if per_person else np.nan  # equal-weight across people
+
         standard_deviation = defaultdict(int)
-        kp_sum = 0
+        df_within = {}
+       
         for channel in channels:
-            for participant in participants_session_data:
-                participant_session_diferences = participants_session_data[participant]["session_differences"]
-                participant_session_averages = participants_session_data[participant]['averages_over_sessions']
-                kp_sum += len(participant_session_diferences) - 1
-                for session in participant_session_diferences:
-                    standard_deviation[channel] += (session[channel] - participant_session_averages[channel])**2
+            num = 0.0
+            df = 0
+            for participant, pdata in participants_session_data.items():
+                Dps = [sd[channel] for sd in pdata["session_differences"]]  # session diffs for this channel
+                k_p = len(Dps)
+                if k_p >= 2:
+                    Dpbar = pdata["averages_over_sessions"][channel]
+                    residuals = np.array(Dps, dtype=float) - float(Dpbar)
+                    num += float((residuals**2).sum())
+                    df  += (k_p - 1)
+                # if k_p == 0 or 1: contribute nothing to num or df
+            standard_deviation[channel] = np.sqrt(num/df) if df > 0 else np.nan
+            df_within[channel] = df
         
-        for channel in channels:
-            standard_deviation[channel] = np.sqrt(standard_deviation[channel]/kp_sum)
-        
-        grand_mean_participants = defaultdict(int)
-        for participant in participants_session_data:
-            participant_session_averages = participants_session_data[participant]['averages_over_sessions']
-            for channel in channels:
-                grand_mean_participants[channel] += participant_session_averages[channel]
-        
-        effect_sizes = {}
-        for channel in grand_mean_participants:
-            grand_mean_participants[channel] = grand_mean_participants[channel] / len(participants_session_data)
-            effect_sizes[channel] = grand_mean_participants[channel] / standard_deviation[channel]
-        
-        channels_mean = defaultdict(lambda: defaultdict(int))
-        channels_std_deviation = defaultdict(lambda: defaultdict(int))
-        channels_median = defaultdict(lambda: defaultdict(int))
-        channels_min = defaultdict(lambda: defaultdict(int))
-        channels_max = defaultdict(lambda: defaultdict(int))
+        # Build per-channel sums and counts
+        grand_sum = {ch: 0.0 for ch in channels}
+        P_ch = {ch: 0 for ch in channels}
 
-        for channel in channels:
-            for data_type in class_instance.data_types:
-                channel_means[channel][data_type] = [values for sublist in channel_means[channel][data_type] for values in sublist]
-                channels_mean[channel][data_type] = np.mean(channel_means[channel][data_type])
-                channels_std_deviation[channel][data_type] = np.std(channel_means[channel][data_type])
-                channels_median[channel][data_type] = np.median(channel_means[channel][data_type])
-                channels_min[channel][data_type] = np.min(channel_means[channel][data_type])
-                channels_max[channel][data_type] = np.max(channel_means[channel][data_type])
+        for pdata in participants_session_data.values():
+            avgs = pdata["averages_over_sessions"]  # dict: channel -> \bar D_{p,u}
+            for ch in channels:
+                val = avgs[ch]
+                if np.isfinite(val):
+                    grand_sum[ch] += float(val)
+                    P_ch[ch] += 1
+
+        # Per-channel grand mean across participants (equal person weight)
+        grand_mean_participants = {
+            ch: (grand_sum[ch] / P_ch[ch]) if P_ch[ch] > 0 else np.nan
+            for ch in channels
+        }
+
+        # Effect size d_within = grand mean / s_within, per channel
+        effect_sizes = {
+            ch: (grand_mean_participants[ch] / standard_deviation[ch])
+                if (np.isfinite(grand_mean_participants[ch]) and np.isfinite(standard_deviation[ch]) and standard_deviation[ch] > 0)
+                else np.nan
+            for ch in channels
+        }
                 
-        return (effect_sizes, channels_mean, channels_std_deviation, channels_median, channels_min, channels_max)
+        return (effect_sizes, grand_mean_participants, standard_deviation, condition_means, df_within, P_ch)
 
-    _keys = ["Effect size", "Channels' means", "Channels' std. deviation", "Channels' medians", "Channels' min", "Channels' max"]
+    _keys = ["Effect size", "Channels' mean difference", "Channels' within-participant SD", "Conditions' mean", "DF within", "P Ch."]
     raw_return_values = _compute_effect_size(raw_epochs, raw_epochs, tmin, tmax, channels)
     preprocessed_return_values = _compute_effect_size(raw_epochs, preprocessed_epochs, tmin, tmax, channels)
     raw_values = {key: val for key, val in zip(_keys, raw_return_values)}
